87044d7ee4c84a9f578ad81cea80fc7a8f89132f
Magnus Haugaard Rønne <magnuskv@gmail.com>
2020-08-05 14:24:53 +0200

    added automatic update of file data and plotting of the figure from command line

--- GCFID.py
+++ GCFID.py
@@ -18,7 +18,7 @@ nDodecane = 0.05218106#Dodecane*0.75/170.33
 
 #List of start and end points of peaks
 #Format: Name, Starting point for integration, End point for integration, Starting Point for Background, End Point for Background,Last end poitn for Integration
-PeakList = {'MeOH' : [1.415, 1.45, 1.40, 1.51, 1.51 ],
+PeakList = {'MeOH' : [1.415, 1.46, 1.4, 1.51, 1.51 ],
     'Phenol' : [5.78, 5.92, 5.5, 6.2, 5.87],
     'PhenylFormate' : [5.92, 6.0, 5.831, 6.2, 6.2],
     'Dodecane' : [6.98, 7.05, 6.9, 7.15, 7.15],
@@ -26,25 +26,27 @@ PeakList = {'MeOH' : [1.415, 1.45, 1.40, 1.51, 1.51 ],
     'MethylFormate': [1.47, 1.50, 1.46, 1.507, 1.505]
     }
 
-
-fileRegex = re.compile(r'(.+\/)(.+)(\.CSV)')
-#files = glob.glob(folder)
-files = TKinterImport.importData()
-
+### File for storing calibration curve data
 CCurve = r'C:\Users\Magnu\Nextcloud\PostDOC\ElectrochemicalHydrogenation\MS\CalibrationCurve.csv'
 CC = pd.read_csv(CCurve)
+
+### Data file Name
+DataFile = r'C:\Users\Magnu\Nextcloud\PostDOC\ElectrochemicalHydrogenation\MS\Data.csv'
+
+#Importing data with TKinter and make a regex to sort the filenames
+fileRegex = re.compile(r'(.+\/)(.+)(\.CSV)')
+files = TKinterImport.importData()
+
 
 plt.figure(figsize=(7,4))
 data=[]
 for i in files:
     FileName = fileRegex.search(i).group(2)
-    #FileName = i.replace(folder[:-7],'')
-    #FileName = FileName[1:-4]
     
     ### Read in Data
     df = BaselineAndArea.ReadFID(i)    
         
-    #Background and integration
+    ### Background and integration
     AreaList = [FileName]
     for keys in PeakList:
         AreaList.append(BaselineAndArea.FindArea(df,PeakList[keys]))
@@ -63,13 +65,18 @@ for i in files:
     #Plot
     plt.plot(df['rt'],df['y'])
 
-# plt.xlim(1.3,1.57)
-# plt.ylim(-0.5,2)
-# plt.title(FileName)
     
 data = pd.DataFrame(data)
 data.columns=['Name','IMeOH','IPhenol','IPhenylFormate','IDodecane','iMystery','MethylFormate','nMeOH','nPhenol','nPhenylFormate','yMeOH','Conversion']
+print(data)
+plt.show()
 
-
-
-data.to_csv(r'C:\Users\Magnu\Nextcloud\PostDOC\ElectrochemicalHydrogenation\MS\Data.csv')
+a = input('Write "y" to store data: ')
+          
+if a == 'y':
+    try:
+        storedData = pd.read_csv(DataFile)
+        storedData = storedData.append(data)
+    except FileNotFoundError:
+        storedData = data     
+    storedData.to_csv(DataFile, index=False)
